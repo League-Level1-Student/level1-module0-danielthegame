a439426bfdddd2198866a0ec2a1bd78fbbf2dfaf
student <student@ip-172-31-51-78.us-west-2.compute.internal>
2021-08-03 18:30:23 -0700

    hi

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,17 @@
 package _01_houses;
 
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
+	private static final 
+	Robot rob = new Robot();
 	public void run() {
+		rob.penDown();
+		rob.setSpeed(50);
+		rob.turn(90);
+		rob.move(1000);
 		
+		rob.setPenColor(Color.green);
 	}
-}
